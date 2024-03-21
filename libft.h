9d651b070aba793c61fcc63bdd2c1ca0378de471
EtienneTessier <etienne.tessier1@gmail.com>
2024-03-21 15:43:08 +0100

    ajout strlen

--- libft.h
+++ libft.h
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isprint.c                                          :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etienne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 09:37:05 by etienne           #+#    #+#             */
-/*   Updated: 2024/03/21 10:20:53 by etienne          ###   ########.fr       */
+/*   Created: 2024/03/21 13:43:50 by etienne           #+#    #+#             */
+/*   Updated: 2024/03/21 15:33:27 by etienne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isprint(int c)
-{
-	if (c >= ' ' && c <= '~')
-		return (1);
-	else
-		return (0);
-}
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+
+int	ft_isdigit(int c);
+
+int	ft_isalnum(int c);
+
+int	ft_isascii(int c);
+
+int	ft_isprint(int c);
+
+int	ft_strlen(char *str);
+
+void    *ft_memset(void *ptr, int x, size_t n);
+#endif
---
+++ main.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etienne <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 13:58:47 by etienne           #+#    #+#             */
+/*   Updated: 2024/03/21 15:18:00 by etienne          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	// ft_isalpha
+	printf("\nft_isalpha :\n");
+	printf("a :%d\n", ft_isalpha('a'));
+	printf("C :%d\n", ft_isalpha('C'));
+	printf("= :%d\n", ft_isalpha('='));
+	printf("0 :%d\n", ft_isalpha('0'));
+	printf("V :%d\n", ft_isalpha('V'));
+	printf(". :%d\n", ft_isalpha('.'));
+
+	// ft_isdigit
+	printf("\nft_isdigit :\n");
+	printf("a :%d\n", ft_isdigit('a'));
+	printf("1 :%d\n", ft_isdigit('1'));
+	printf("= :%d\n", ft_isdigit('='));
+	printf("0 :%d\n", ft_isdigit('0'));
+	printf("V :%d\n", ft_isdigit('V'));
+	printf(". :%d\n", ft_isdigit('.'));
+
+	// ft_isalnum
+	printf("\nft_isalnum :\n");
+	printf("a :%d\n", ft_isalnum('a'));
+	printf("C :%d\n", ft_isalnum('C'));
+	printf("= :%d\n", ft_isalnum('='));
+	printf("0 :%d\n", ft_isalnum('0'));
+	printf("V :%d\n", ft_isalnum('V'));
+	printf(". :%d\n", ft_isalnum('.'));
+
+	// ft_isascii
+	printf("\nft_isascii :\n");
+	printf("-1 :%d\n", ft_isascii(-1));
+	printf("C :%d\n", ft_isascii('C'));
+	printf("= :%d\n", ft_isascii('='));
+	printf("0 :%d\n", ft_isascii('0'));
+	printf("128 :%d\n", ft_isascii(128));
+	printf(". :%d\n", ft_isascii('.'));
+
+	// ft_isprint
+	printf("\nft_isprint :\n");
+	printf("\\n :%d\n", ft_isprint('\n'));
+	printf("C :%d\n", ft_isprint('C'));
+	printf("= :%d\n", ft_isprint('='));
+	printf("0 :%d\n", ft_isprint('0'));
+	printf("DEL :%d\n", ft_isprint(127));
+	printf(". :%d\n", ft_isprint('.'));
+
+	// ft_strlen
+	printf("\nft_strlen :\n");
+	printf("Je suis content:%d\n", ft_strlen("Je suis content"));
+	printf(":%d\n", ft_strlen(""));
+	printf("Je suis:%d\n", ft_strlen("Je suis"));
+
+	// ft_memset
+	printf("\nft_memset :\n");
+	char *str = "bonjour";
+	ft_memset(str + 3, '.', 2*sizeof(char));
+	printf("bonjour :%s\n", str);
+}
